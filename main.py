bd899a17888bc8312aeb5f93ec2ca80b28852439
Behcet <tasbehcet34@gmail.com>
2025-04-24 21:41:32 +0300

    .

---
+++ main.py
@@ -0,0 +1,22 @@
+# %%
+print("Derin ogrenme algoritmalari birinci hücre")
+
+# %%
+
+print("Derin ogrenme algoritmalari ikinci hücre")
+
+
+# %%
+print("Derin ogrenme algoritmalari ucuncu hücre")
+
+
+# %%
+print("Derin ogrenme algoritmalari dorduncu hücre")
+
+
+# %%
+import torch
+import torch.version
+
+# %%
+print(torch.__version__)
